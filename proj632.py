98616f8e770218ba3844603c0a77b26bde6928c2
Simon Guilbert <simonpolytech@gmail.com>
2020-03-12 10:54:12 +0100

    Update proj632.py

--- proj632.py
+++ proj632.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 12 10:06:47 2020
-
 @author: Jonathan Molieres
 """
 from urllib.request import Request
@@ -10,7 +9,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 url="https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=restaurant&ou=haute-savoie-74"
 req=Request(url,headers={"User-Agent":"Mozilla/70.0"})
-html = urlopen(req).read()
+html = urlopen(req)
 html_soup = BeautifulSoup(html, 'html.parser')
 rows = html_soup.findAll("option")
 page=[]
