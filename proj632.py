5c6a69ea4b5a086ff57631078d631d13bd740fad
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2020-03-12 11:05:05 +0100

    recuperation nombre de page

--- proj632.py
+++ proj632.py
@@ -9,12 +9,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 url="https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=restaurant&ou=haute-savoie-74"
 req=Request(url,headers={"User-Agent":"Mozilla/70.0"})
-html = urlopen(req)
+html = urlopen(req).read()
 html_soup = BeautifulSoup(html, 'html.parser')
 rows = html_soup.findAll("option")
 page=[]
 print(len(rows))
 for row in rows:
-    cells = row.findAll()
-    page.append(cells)
+    try:
+        page.append(int(row["value"]))
+    except:
+        pass
 print(page)
