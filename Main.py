3cc95482329fd0b44b6ce38ab48178bc74d430c5
Jonathan-molieres <56953324+Jonathan-molieres@users.noreply.github.com>
2020-03-16 11:14:45 +0100

    Add files via upload

--- Main.py
+++ Main.py
@@ -13,11 +13,11 @@ import csv
 # Fonction
 # =============================================================================
 def to_CSV(donne):
-    with open("restaurant.csv",'w') as csvfile:
+    with open("data/restaurant.csv",'w') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=' ',quotechar=';',quoting=csv.QUOTE_MINIMAL)
         for nom_restaurant in donne.keys():
-            print([nom_restaurant]+donne[nom_restaurant])
             spamwriter.writerow([nom_restaurant]+donne[nom_restaurant])
+    print("CSV terminé à l'emplacement data/restaurant.csv du repertoire courant")
 # =============================================================================
 # Variable
 # =============================================================================
@@ -25,11 +25,9 @@ url_acceuil="https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=restaur
 donne={}
 if __name__ == "__main__":
     nbre_page=TPJ.nombre_de_page(url_acceuil)
-    for page in range(1,3):
+    for page in range(1,nbre_page+1):
         url_page=TPJ.visiter_page(url_acceuil, page)
         donne.update(TPJ.recuperation_des_json(url_page))
-    print(len(donne))
-
     # Creaction du CSV
     to_CSV(donne)
         
--- TraitementPagesJaunes.py
+++ TraitementPagesJaunes.py
@@ -24,12 +24,12 @@ def nombre_de_page(url):
     Parameters
     ----------
     url : TYPE:String
-        DESCRIPTION.
+        DESCRIPTION: Lien de la page d'acceuil du sit des pages Jaunes
 
     Returns
     -------
-    TYPE
-        DESCRIPTION.
+    TYPE: int
+        DESCRIPTION: nombre de page possible
     """
     page=[]
     while len(page)==0:#attend de reponse
@@ -45,25 +45,57 @@ def nombre_de_page(url):
     return len(page)
 
 def visiter_page(url,page):
-    """Creer les url pour changer de page"""
+    """Creer les url pour changer de page
+    Parameters
+    ----------
+    url : TYPE
+        DESCRIPTION.
+    page : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION:Lien avec la page
+    """
     return url+"&page="+str(page)
 
 # =============================================================================
 # Traitement d'un restaurant
 # =============================================================================
+
+def recherche_site_web(resto):
+    '''
+    Crée la liste des sites web des restaurants dont les identifiants sont donnés en paramètres
+    Si le site n'existe pas ou n'est pas renseigner alors on ajout "NONE" à la liste
+    :param id_resto: la liste des identifiants de chaque restaurant
+    :return: la liste des site web des restaurants correspondant aux identifiants
+    '''
+
+    url = "https://www.pagesjaunes.fr/pros/detail?bloc_id=" + str(resto)
+    req = Request(url, headers={"User-Agent": "Mozilla/74.0"})
+    html = urlopen(req).read()
+    html_soup = BeautifulSoup(html, 'html.parser')
+    div_sites=html_soup.find("div" , class_="lvs-container marg-btm-s") # div contenant l'adresse du site web
+    try:
+        for div in div_sites:
+            site=div.find('span', class_="value") # Recherche de l'adresse du site web
+            return site.text
+    except :
+        return "None"
     
 def recuperation_des_json(url):
     """
     recuperation des donnee du restaurant en passant par le json que les pages Jaunes proposent
     Parameters
     ----------
-    url : TYPE
-        DESCRIPTION.
+    url : TYPE:String
+        DESCRIPTION:Lien de la page de recherche
 
     Returns
     -------
-    donne : TYPE
-        DESCRIPTION.
+    donne : TYPE:dictionary
+        DESCRIPTION: Information retenu pour le CSV
 
     """
     req=Request(url,headers={"User-Agent":"Mozilla/70.0"})
@@ -77,15 +109,17 @@ def recuperation_des_json(url):
     print("nombre de restaurant sur la page=",len(id_resto))
     donne={}
     for resto in id_resto:
-        url="https://www.pagesjaunes.fr/pros/detail?bloc_id="+str(resto)
+        url="https://www.pagesjaunes.fr/pros/detail?bloc_id="+str(resto)#accés au differents lien
         req=Request(url,headers={"User-Agent":"Mozilla/70.0"})
         html = urlopen(req).read()
         html_soup = BeautifulSoup(html, 'html.parser')
-        recherche = html_soup.findAll("script",attrs={'type':"application/ld+json"})
-        
+        #recherche site 
+        nom_site=recherche_site_web(resto)
+        #recherche json
+        recherche = html_soup.findAll("script",attrs={'type':"application/ld+json"})   
         try:
             recherche=json.loads(recherche[0].getText())[0]
-            donne[recherche['name']]=[recherche["address"]["streetAddress"],recherche["address"]["postalCode"],recherche["address"]["addressLocality"],recherche["telephone"]]
+            donne[recherche['name']]=[recherche["address"]["streetAddress"],recherche["address"]["postalCode"],recherche["address"]["addressLocality"],recherche["telephone"],nom_site]
         except:
             pass
     return donne
