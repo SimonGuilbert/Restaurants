80b2ca625bd814af63a45b3c4c8707b6249cd050
Simon Guilbert <simonpolytech@gmail.com>
2020-04-26 21:06:47 +0200

    Tri des fonctions utilisées

--- Folium.py
+++ Folium.py
@@ -8,53 +8,18 @@ Created on Thu Feb 20 17:42:14 2020
 # import
 # =============================================================================
 import folium as fl
-import json
-import requests
 
 
 # =============================================================================
 # Class
 # =============================================================================
 class Carte:
-    """classe gerant la carte avec folium"""
-
     def __init__(self, coord=None):
-        """
-        Parameters
-        ----------
-        coord :TYPE: Liste , optional.
-            DESCRIPTION:contient les coordonnees de depart. The default is [48.8534,2.3488] qui correspond à Paris.
-        Returns
-        -------
-        None.
-        """
+
+        # Coordonnées par défaut = [45.89912, 6.12871] qui correspondent à Annecy
         if coord is None:
             coord = [45.89912, 6.12871]
         self.carte = fl.Map(location=coord,zoom_start=6)
-
-    def cercle(self, coord, rayon, color='crimson'):
-        """
-        Permet d'ajouter un cercle à la carte
-        Parameters
-        ----------
-        coord : TYPE: Liste de floattant
-            DESCRIPTION: Liste des coordonnees
-        rayon : TYPE: entier
-            DESCRIPTION: determine le rayon du cercle
-        color : TYPE:String , optional
-            DESCRIPTION: Definie la couleur du cercle,The default is 'crimson'.
-
-        Returns
-        -------
-        None.
-        """
-        fl.Circle(
-            radius=rayon,
-            location=coord,
-            popup='The Waterfront',
-            color=color,
-            fill=True,
-            fill_color=color).add_to(self.carte)
 
     def marqueur(self, coordonnee, popupstr, tooltip):
         """
@@ -64,45 +29,14 @@ class Carte:
         coordonnee : TYPE:Liste
             DESCRIPTION: Liste des coordonnees 
         tooltip : TYPE: String 
-            DESCRIPTION:String visible
+            DESCRIPTION: Message lorsque la souris passe sur un marqueur
         popupstr : TYPE: String
-            DESCRIPTION: String visible lorsqu'on clique sur le marqueur
+            DESCRIPTION: Message lorsqu'on clique sur le marqueur
 
-        Returns
-        -------
-        None.
         lien pour modifier l'apparence des icones : getbootstrap.com/docs/3.3/components/
         """
         fl.Marker(coordonnee, popup='<i>' + str(popupstr) + '</i>', tooltip=tooltip,
                   icon=fl.Icon(color='black', icon_color='orange', icon='glyphicon-cutlery')).add_to(self.carte)
 
     def save(self, fil='index.html'):
-        """
-        Fonction de creaction du html
-        Parameters
-        ----------
-        fil : TYPE: String, optional
-            DESCRIPTION: titre du fichier The default is 'index.html'.
-
-        Returns
-        -------
-        None.
-        """
-        self.carte.save("html\\" + fil)
-
-    def polygon(self, locations, tooltip=None, popup=True):
-        """
-        Trace un polygon sur la carte.
-        Parameters
-        ----------
-        locations : TYPE: Liste  des coordonnees
-            DESCRIPTION:
-        tooltip : TYPE: String visible sur la carte, optional
-            DESCRIPTION: phrase à ajouter.The default is None.
-        popup : TYPE: String visible lorsqu'on clique sur le polygon,optional
-            DESCRIPTION. The default is True.
-        Returns
-        -------
-        None.
-        """
-        fl.Polygon(locations, popup, tooltip).add_to(self.carte)
+        self.carte.save("Resultats/" + fil) # Enregistre la carte dans le dossier Resultats
